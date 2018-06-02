2e9e678aed7e610b7afb61e5d7ade58d498c35d3
Sebastian Bohmann <mail@sebastian-bohmann.com>
2018-06-02 12:19:27 +0200

    PathHandler

--- src/ceejay/CeeJay.java
+++ src/ceejay/CeeJay.java
@@ -1,9 +1,16 @@
 package ceejay;
 
 import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.ResourceService;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.HandlerCollection;
-import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.resource.Resource;
+
+import java.io.File;
+import java.io.IOException;
 
 class CeeJay {
     public static void main(String[] args) throws Exception {
@@ -16,26 +23,27 @@ class CeeJay {
 
     private void createServer() throws Exception {
         Server server = new Server(8080);
-        server.setHandler(createHandlerCollection());
+        server.setHandler(createServletHandler());
         server.start();
         server.join();
     }
 
-    private HandlerCollection createHandlerCollection() {
-        HandlerCollection result = new HandlerCollection();
-        result.setHandlers(createHandlerArray());
+    private Handler createServletHandler() throws IOException {
+        ServletContextHandler result = new ServletContextHandler();
+        result.setBaseResource(Resource.newResource(staticResourceDirectory()));
+        addDefaultServlet(result);
         return result;
     }
 
-    private Handler[] createHandlerArray() {
-        return new Handler[]{
-                new RequestHandler(),
-                createResourceHandler()};
+    private File staticResourceDirectory() throws IOException {
+        File result = new File("static");
+        if (!result.isDirectory()) {
+            throw new IOException("Not a directory: [" + result.getCanonicalPath() + "]");
+        }
+        return result;
     }
 
-    private Handler createResourceHandler() {
-        ResourceHandler result = new ResourceHandler();
-        result.setResourceBase("static");
-        return result;
+    private void addDefaultServlet(ServletContextHandler serlvetHandler) {
+        serlvetHandler.addServlet(new ServletHolder(new DefaultServlet()), "/");
     }
 }
